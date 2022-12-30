0d4b5af92708d68fff647ff36d638040363f9394
Stojance Nikolov <stojance_nikolov@digit.net.mk>
2022-12-30 11:02:55 +0100

    sn

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
-
+import { AuthenticationGuard } from '../auth/authentication.guard';
 import { Shell } from '@app/shell/shell.service';
 import { CustomerComponent } from './customer.component';
 
 const routes: Routes = [
-  Shell.childRoutes([{ path: 'customer', component: CustomerComponent, data: { title: marker('Customer') } }]),
+  Shell.childRoutes([
+    {
+      path: 'customer',
+      component: CustomerComponent,
+      data: { title: marker('Customer') },
+      canActivate: [AuthenticationGuard],
+    },
+  ]),
 ];
 
 @NgModule({
